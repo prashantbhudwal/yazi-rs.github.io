9aa693e0d43e4b63daf960a78fac2661a97fe566
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-02-05 23:56:49 +0530

    Fix video paths in features page

--- src/pages/features/index.tsx
+++ src/pages/features/index.tsx
@@ -5,39 +5,39 @@ import { Header, Cards, CardProps } from "@site/src/components/Highlights"
 const features: CardProps[] = [
 	{
 		title: "Scrollable Preview",
-		video: "/videos/scrollable-preview.mp4",
+		video: "videos/scrollable-preview.mp4",
 		description: "Preview various types of files, and scroll the content of file while previewing.",
 	},
 	{
 		title: "Visual Mode & Bulk Rename",
-		video: "/videos/visual-mode_bulk-rename.mp4",
+		video: "videos/visual-mode_bulk-rename.mp4",
 		description: "Bulk select files in visual mode, and rename them.",
 	},
 	{
 		title: "Vim-like Input & Select Component",
-		video: "/videos/input_select.mp4",
+		video: "videos/input_select.mp4",
 		description: "Quickly edit filename in the Input, and choose how to open it in the Select.",
 	},
 	{
 		title: "Multi-Tab & fzf, zoxide",
-		video: "/videos/multi-tab_zoxide.mp4",
+		video: "videos/multi-tab_zoxide.mp4",
 		description: "Collaborate across multiple tabs, and use fzf, zoxide for quick jumps.",
 	},
 	{
 		title: "Multi-Select & Task Management",
-		video: "/videos/multi-select_task-management.mp4",
+		video: "videos/multi-select_task-management.mp4",
 		description:
 			"Select multiple files individually, perform copy, cut, etc. Which are scheduled by the task system, providing real-time progress reports and task cancellation.",
 	},
 	{
 		title: "Incremental Find",
-		video: "/videos/incremental-find.mp4",
+		video: "videos/incremental-find.mp4",
 		description:
 			"Find files incrementally in real-time, with the current position and number of all matches displayed.",
 	},
 	{
 		title: "Search",
-		video: "/videos/search.mp4",
+		video: "videos/search.mp4",
 		description: "Search by name using fd, by content using rg, and perform arbitrary operations on the results.",
 	},
 ]
